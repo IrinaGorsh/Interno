2e0b93a0575f1bff37f4b1f72bac292b619e79aa
Irina <gorshenevairina@googlemail.com>
2024-01-25 21:15:05 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template>  
+<section class="intro">
+        <div class="banner container">
+            <div class="banner__wrapper">
+                <div class="banner__info">
+                    <h1 class="banner__heading">Let Your Home Be Unique</h1>
+                    <p class="banner__desc">There are many variations of the passages of lorem Ipsum fromavailable, majority.</p>
+                    <button class="start__btn"> <div class="start__btn__txt">Get Started<svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" viewBox="0 0 18 17" fill="none" >
+                        <path d="M1 8.43542L14.7232 8.29857M9.61818 1.91138L16.1412 8.43436L9.48677 15.0887" stroke="#CDA274" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"/>
+                      </svg></div></button>
+                    </div>  
+            </div>          
+             
+        </div>
+    </section> 
+    
+  <BlogPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlogPage from './components/BlogPage.vue';
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BlogPage
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/BlogPage.vue
@@ -0,0 +1,157 @@
+<template>
+    <section class="post__details">
+        
+        <div class="post__content container">
+            
+<div class="content__column">
+    <h1 class="content__heading">Let’s Get Solution for Building  Construction Work</h1>    
+        <div class="content__img top"></div>  
+        <div class="content__date">
+            <p class="current__date">26 December,2022</p> 
+            <div class="categories">
+                <a href="#" class="cat">Interior</a>
+                <a href="#" class="cat">/ Home</a>
+                <a href="#" class="cat">/ Decor</a>
+            </div>
+        
+        </div>
+        <p class="content">
+            Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable. <br><br>
+        Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.
+        </p>
+        <div class="quotes">
+            <div class="quote__wrapper">
+                <p class="brackets">“</p>
+                <p class="quote">The details are not the details. They make the design.</p>
+            </div>
+        
+        </div>
+        <h3 class="content__subt">Design sprints are great</h3>
+        <p class="content">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered.</p>
+        <p class="content fn"><span class="footnote">1</span>Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered.</p>
+        <p class="content fn"><span class="footnote">2</span>Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered.</p>
+        <p class="content fn"><span class="footnote">3</span>Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered.</p>
+        <div class="content__img bottom"></div>
+        <p class="content">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered.</p>
+    
+</div>
+
+<div class="tags__column">
+    <h4 class="tags__title">Tags</h4>
+    <div class="tags__wrapper">
+        <div class="tag">
+            <a href="#" class="tag__link">Kitchen</a>        
+        </div>
+        <div class="tag">
+            <a href="#" class="tag__link" >Living</a>        
+        </div>
+        <div class="tag">
+            <a href="#" class="tag__link">Interior</a>        
+        </div>
+        
+       
+       
+    </div>   
+    
+</div>
+
+</div>
+</section>
+    <div class="secondblog container">
+    <div class="blog__entry"  v-for="entry in entrys" :key= "entry.id">
+                <div class="entry__img"  style=" background: url(require ('../assets/images/SetPhoto4.jpg')), lightgray 50% / cover; padding: 228px 196px 20px 20px">
+                    <div class="entry__tag">
+                        <a href="#" class="tag__txt">{{entry.tag}}</a>                       
+                    </div>                  
+                
+                </div>
+
+                <div class="entry__sub">
+                    <h4 class="entry__title">{{entry.title}}</h4>
+                    <div class="entry__open">
+                        <p class="entry__date">{{entry.date}}</p>
+                        <div class="entry__link__wrapper">
+                            <a href="#" class="entry__link"><svg xmlns="http://www.w3.org/2000/svg" width="52" height="53" viewBox="0 0 52 53" fill="none"  class="entry__svg">
+                                <circle cx="26" cy="26.267" r="26" fill="#F4F0EC"  class="entry__circ"/>
+                                <path d="M23.7714 32.9527L29.7143 26.267L23.7714 19.5813" stroke="#292F36" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                              </svg></a>
+                        </div>
+                       
+                    </div>
+                </div>            
+                                    
+        
+            </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+        return{
+			entrys:[
+				{
+				id: 1,
+                //img: '@/assets/images/Kitchen2.jpg',
+				tag: "Kitchen",
+				title:"Let’s Get Solution For Building Construction Work",
+				date: '26 December,2022',
+				
+			},
+
+			{
+				id: 2,
+                //img: '@/assets/images/Kitchen2.jpg',
+				tag: "Living",
+				title:"Low Cost Latest Invented Interior Designing Ideas.",
+				date: '22 December,2022',
+				
+			},
+
+			{
+				id: 3,
+                //img: '@/assets/images/Kitchen2.jpg',
+				tag: "Interior",
+				title:"Best For Any Office & Business Interior Solution",
+				date: '25 December,2022',
+				
+			},		
+
+            {
+				id: 4,
+                //img: '@/assets/images/Kitchen2.jpg',
+				tag: "Interior",
+				title:"Low Cost Latest Invented Interior Designing Ideas",
+				date: '25 December,2022',
+				
+			},		
+
+            {
+				id: 5,
+                //img: '@/assets/images/Kitchen2.jpg',
+				tag: "Kitchen",
+				title:"Best For Any Office & Business Interior Solution",
+				date: '25 December,2022',
+				
+			},		
+
+            {
+				id: 6,
+                //img: '@/assets/images/Kitchen2.jpg',
+				tag: "Living",
+				title:"Let’s Get Solution For Building Construction Work",
+				date: '25 December,2022',
+				
+			},		
+			
+		],
+		};
+    },
+
+    
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
